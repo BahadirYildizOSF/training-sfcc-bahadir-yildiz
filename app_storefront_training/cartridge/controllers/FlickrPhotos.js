d88d44de356df3ebc560ea168137d054732b73c1
Bahadir Yildiz <bahadir.yildiz@osf-commerce.com>
2019-03-04 14:05:48 +0300

    renewed metadata files, created controller and templates, implemented the httclient script and sent the data to template for rendering

---
+++ app_storefront_training/cartridge/controllers/FlickrPhotos.js
@@ -0,0 +1,28 @@
+var ISML = require('dw/template/ISML');
+var guard = require('app_storefront_controllers/cartridge/scripts/guard');
+var HTTPClient = require('dw/net/HTTPClient');
+var Response = require('app_storefront_controllers/cartridge/scripts/util/Response');
+
+function start() {
+    var format = 'json',
+        preferences = dw.system.Site.getCurrent().getPreferences(),
+        lang = preferences.getCustom()["flickrLanguage"];
+    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=" + format + "&lang=" + lang;
+    var client = new HTTPClient();
+    client.setRequestHeader('Content-Type', 'application/json');
+    client.open('GET', flickrAPI);
+    client.setTimeout(3000);
+    client.send();
+    if (client.statusCode == 200) {
+        var data = eval(client.text);
+        ISML.renderTemplate("flickr/showphotos", {
+            items: data.items
+        })
+    }
+}
+
+function jsonFlickrFeed(data){
+    return data;
+}
+
+exports.Start = guard.ensure(["get"], start);
