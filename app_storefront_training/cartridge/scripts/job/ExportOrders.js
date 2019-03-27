a70aa2f882f6714face371479f378ad11d54a489
Bahadir Yildiz <bahadir.yildiz@osf-commerce.com>
2019-03-27 17:14:44 +0300

    Moved DW API includes to top and changed the return type

--- app_storefront_training/cartridge/scripts/job/ExportOrders.js
+++ app_storefront_training/cartridge/scripts/job/ExportOrders.js
@@ -1,21 +1,23 @@
+var File = require('dw/io/File');
+var FileWriter = require('dw/io/FileWriter');
+var XMLStreamWriter = require('dw/io/XMLStreamWriter');
+var Transaction = require('dw/system/Transaction');
+var OrderMgr = require('dw/order/OrderMgr');
+var Order = require('dw/order/Order');
+var Status = require('dw/system/Status');
+
 /**
  * Export the product which haven't been exported.
+ * @returns {dw.system.Status}
  */
-
 exports.execute = function (params) {
-    const File = require('dw/io/File');
-    const FileWriter = require('dw/io/FileWriter');
-    const XMLStreamWriter = require('dw/io/XMLStreamWriter');
-    const Transaction = require('dw/system/Transaction');
-    const OrderMgr = require('dw/order/OrderMgr');
-    const Order = require('dw/order/Order');
     var searchResults = OrderMgr.searchOrders('exportStatus ={0} OR exportStatus ={1}', null, Order.EXPORT_STATUS_NOTEXPORTED, Order.EXPORT_STATUS_READY);
 
     Transaction.wrap(function () {
-        const file = new File(File.IMPEX + File.SEPARATOR + "src" + File.SEPARATOR + "exports" + File.SEPARATOR + "exported_orders.xml");
-        const fileWriter = new FileWriter(file, "UTF-8");
+        var file = new File([File.IMPEX, "src", "exports", "exported_orders.xml"].join(File.SEPERATOR));
+        var fileWriter = new FileWriter(file, "UTF-8");
         fileWriter.setLineSeparator('\r\n');
-        const xsw = new XMLStreamWriter(fileWriter);
+        var xsw = new XMLStreamWriter(fileWriter);
 
         xsw.writeStartDocument();
         xsw.writeStartElement("orders");
@@ -32,58 +34,58 @@ exports.execute = function (params) {
              */
             xsw.writeStartElement("order");
             xsw.writeAttribute("order-no", order.getOrderNo());
-                if(orderDate !== null){
+                if(orderDate !== null) {
                     xsw.writeStartElement("order-date");
                         xsw.writeCharacters(order.getCreationDate().toString());
                     xsw.writeEndElement();
                 }
                 xsw.writeStartElement("customer");
-                    if(order.getCustomerName() !== null){
+                    if(order.getCustomerName() !== null) {
                         xsw.writeStartElement("customer-name");
                             xsw.writeCharacters(order.getCustomerName());
                         xsw.writeEndElement();
                     }
-                    if(order.getCustomerNo() !== null){
+                    if(order.getCustomerNo() !== null) {
                         xsw.writeStartElement("customer-no");
                             xsw.writeCharacters(order.getCustomerNo());
                         xsw.writeEndElement();
                     }
-                    if(order.getCustomerEmail() !== null){
+                    if(order.getCustomerEmail() !== null) {
                         xsw.writeStartElement("customer-email");
                             xsw.writeCharacters(order.getCustomerEmail());
                         xsw.writeEndElement();
                     }
                 xsw.writeEndElement();
                 xsw.writeStartElement("adjusted-merchandize-total");
-                    if(order.getAdjustedMerchandizeTotalGrossPrice() !== null){                 
+                    if(order.getAdjustedMerchandizeTotalGrossPrice() !== null) {
                         xsw.writeStartElement("gross-price");
                             xsw.writeCharacters(order.getAdjustedMerchandizeTotalGrossPrice());
-                        xsw.writeEndElement();   
+                        xsw.writeEndElement();
                     }
                 xsw.writeEndElement();
                 xsw.writeStartElement("product-lineitems");
-                    while(productLineItems.hasNext()){
+                    while(productLineItems.hasNext()) {
                         var product = productLineItems.next().getProduct();
-                        if(product !== null){
+                        if(product !== null) {
                             xsw.writeStartElement("product-lineitem");
                                 xsw.writeStartElement("product-id");
                                     xsw.writeCharacters(product.getID());
                                 xsw.writeEndElement();
                             xsw.writeEndElement();
                         }
-                    };
+                    }
                 xsw.writeEndElement();
                 xsw.writeStartElement("payments");
-                    while(paymentInstruments.hasNext()){
+                    while(paymentInstruments.hasNext()) {
                         var paymentInstrument = paymentInstruments.next();
                         xsw.writeStartElement("payment");
                             xsw.writeStartElement("payment-method");
                                 xsw.writeCharacters(paymentInstrument.getPaymentMethod());
                             xsw.writeEndElement();
                         xsw.writeEndElement();
-                    };
+                    }
                 xsw.writeEndElement();
-            xsw.writeEndElement();;
+            xsw.writeEndElement();
             /**
              * @formatter:on
              */
@@ -95,5 +97,5 @@ exports.execute = function (params) {
         fileWriter.close();
     });
 
-    return PIPELET_NEXT;
-}
+    return new Status(Status.OK);
+};
