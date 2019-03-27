b232e45435b16b6af1587353842512e6c98b3e5b
Bahadir Yildiz <bahadir.yildiz@osf-commerce.com>
2019-03-27 11:31:51 +0300

    Reworked on controller, some modifications were squashed from previous git operations

--- app_storefront_training/cartridge/controllers/FlickrPhotos.js
+++ app_storefront_training/cartridge/controllers/FlickrPhotos.js
@@ -1,31 +1,28 @@
 var ISML = require('dw/template/ISML');
 var guard = require('app_storefront_controllers/cartridge/scripts/guard');
 var HTTPClient = require('dw/net/HTTPClient');
-var Resource = require('dw/web/Resource');
+var preferences = require('dw/system/Site').getCurrent().getPreferences();
+
 function start() {
     var format = 'json',
-        preferences = dw.system.Site.getCurrent().getPreferences(),
         lang = preferences.getCustom()["flickrLanguage"];
-    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=" + format + "&lang=" + lang;
+    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?nojsoncallback=1format=" + format + "&lang=" + lang;
     var client = new HTTPClient();
     client.setRequestHeader('Content-Type', 'application/json');
     client.open('GET', flickrAPI);
     client.setTimeout(3000);
     client.send();
-    if (client.statusCode == 200) {
-        var data = eval(client.text);
+
+    if (client.statusCode === 200) {
+        var data = JSON.parse(client.text);
         ISML.renderTemplate("flickr/showphotos", {
             items: data.items
-        })
+        });
     } else {
         ISML.renderTemplate("flickr/showphotos", {
             items: null
-        })
+        });
     }
-}
-
-function jsonFlickrFeed(data){
-    return data;
 }
 
 exports.Start = guard.ensure(["get"], start);
